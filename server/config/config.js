c96213eea99df287ab51c4e45bc540238aaeba53
pssslearning <psanchosopranis@yahoo.es>
2019-05-02 16:31:39 +0200

    Variable de entorno MONGO_URI (Heroku)

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://pssslearning:Kt7GR51GiiOjLBd6@cluster0-fxykp.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
